2fad8b74528f5ea7d759f339b205bc3e94aba383
AdityaTiwari1052 <at9997152@gmail.com>
2024-12-06 13:03:27 +0530

    StateHandling

--- REACT WORK/reactviteapp/src/App.jsx
+++ REACT WORK/reactviteapp/src/App.jsx
@@ -1,13 +1,57 @@
 import React from 'react'
 import Student from './Student'
-
+import StateHandling from './StateHandling'
+                                       //part of businees logic
 function App() {
-  return (
+   
+  // const studentdata=[{
+  
+  //   image:photo,
+  //   name:"Saurabh Yadav",
+  //   roll:"145",
+  //   college:"ABES"
+  // },
+  //   {
+  //     image:photo,
+  //   name:"Sachin Yadav",
+  //   roll:"146",
+  //   college:"ABES"
+  //   },
+    
+
+  //  { image:photo,  
+  //   name:"Gaurav Yadav",
+  //   roll:"147",
+  //   college:"ABES"  
+  //  }
+  
+
+  // ]
+  return(
     <div>
       <div>
-        <h2 style={{color:'red'}}>Hello React Js</h2>
-        <div>
-          <Student  name="Aditya Tiwari" roll="26" college="ABES Engineering College"/>
+        <h2 style={{color:'blue'}}>Hello React JS</h2>
+             <div>
+             {/* {
+               {
+                studentdata.map((ele)=>{
+                  return(
+                     <Student data ={ele}/>
+                  )
+                })
+                 }
+                } */}
+            {/* //  < Student data={studentdata}/> */}
+              </div>  
+
+
+              {/* Here Start StateHandling */}
+              <div>
+                <StateHandling/>
+              </div>
+           
+          <div>
+           {/* <Student image={<img src={photo} height={100} width={100} />} name="Saurabh"  roll="145"  college="ABESEC"/>          way to use component */}
         </div>
       </div>
     </div>
